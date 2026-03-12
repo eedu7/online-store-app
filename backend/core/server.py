7d1fded9a3d714dd93a016edeb6d17dd23e93000
Mueed Ahmad <mueedahmad067@gmail.com>
2026-03-12 09:10:37 +0500

    Added the compose file

---
+++ backend/core/server.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+
+def run_server() -> FastAPI:
+    app_ = FastAPI()
+
+    return app_
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,3 @@
-def main():
-    print("Hello from backend!")
+from core.server import run_server
 
-
-if __name__ == "__main__":
-    main()
+app = run_server()
