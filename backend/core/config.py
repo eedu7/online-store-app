fa0579ff26665531dd4cbc8e9d9c6d2042a06745
Mueed Ahmad <mueedahmad067@gmail.com>
2026-03-12 10:13:58 +0500

    Added the alembic, configuration file

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,7 +1,47 @@
-from pydantic_settings import BaseSettings
+from enum import StrEnum
+
+from pydantic import PostgresDsn, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Config(BaseSettings): ...
+class Environment(StrEnum):
+    DEVELOPMENT = "development"
+    PRODUCTION = "production"
+    TESTING = "testing"
+    STAGING = "staging"
+
+
+class Config(BaseSettings):
+    # Project Environment
+    ENVIRONMENT: Environment = Environment.DEVELOPMENT
+
+    # Database
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = "my_db"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "password@123"
+
+    DB_POOL_SIZE: int = 5
+    DB_POOL_RECYCLE: int = 3600
+    DB_POOL_PRE_PINT: bool = True
+    DB_MAX_OVERFLOW: int = 10
+
+    @computed_field
+    @property
+    def DATABASE_URL(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=True, extra="ignore"
+    )
 
 
 config = Config()
