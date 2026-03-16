b12eee1d2c9eb7733b67390cc983b7bdc61ec708
Mueed Ahmad <mueedahmad067@gmail.com>
2026-03-16 21:28:58 +0500

    feat: added the RoleController

--- backend/app/controllers/__init__.py
+++ backend/app/controllers/__init__.py
@@ -1,4 +1,5 @@
 from .auth import AuthController
+from .role import RoleController
 from .users import UserController
 
-__all__ = ["UserController", "AuthController"]
+__all__ = ["UserController", "AuthController", "RoleController"]
--- backend/app/controllers/role.py
+++ backend/app/controllers/role.py
@@ -1,1 +1,8 @@
+from app.models import DBRole
+from app.repositories import RoleRepository
+from core.controller import BaseController
 
+
+class RoleController(BaseController[DBRole]):
+    def __init__(self, repository: RoleRepository) -> None:
+        super().__init__(DBRole, repository)
