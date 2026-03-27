47a21d67059774326bdef2069f599111da5cdf6a
Mueed Ahmad <mueedahmad067@gmail.com>
2026-03-27 10:43:17 +0500

    feat: updated the DashboardLayout added the notound page, when user is customer

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { requireAuth } from "@/features/auth/auth.utilts";
 
 export default async function Layout({
@@ -5,6 +6,12 @@ export default async function Layout({
   tenant,
 }: Readonly<{ admin: React.ReactNode; tenant: React.ReactNode }>) {
   const user = await requireAuth();
+
+  const isCustomer = user.roles.some((role) => role.name === "customer");
+
+  if (isCustomer) {
+    notFound();
+  }
 
   const isAdmin = user.roles.some((role) => role.name === "admin");
 
