31b424154e5ab48de76f5110d5a48c4277e06133
Mueed Ahmad <mueedahmad067@gmail.com>
2026-03-28 14:51:07 +0500

    feat: added the useRoles hooks

--- frontend/src/features/roles/hooks/use-roles.ts
+++ frontend/src/features/roles/hooks/use-roles.ts
@@ -1,26 +1,19 @@
+import {useQuery} from "@tanstack/react-query";
+import type {Role} from "@/features/roles/roles.types";
+import {apiServerClient} from "@/lib/api/api.server";
 
+const createRole = () => {};
+const getRoles = () =>
+  useQuery({
+    queryFn: async (): Promise<Role[]> => {
+      return await apiServerClient("/roles", {
+        method: "GET",
+      });
+    },
+    queryKey: ["roles", "use-roles"],
+  });
+const getRoleById = () => {};
+const updateRole = () => {};
+const deleteRole = () => {};
 
-
-export const useRoles = () => {
-
-    const createRole = () => {
-        
-    }
-    const getRole = () => {}
-    const getRoleById = () => {}
-    const getRoles = () => {}
-    const updateRole = () => {}
-    const deleteRole = () => {}
-
-    return {
-        createRole,
-        getRole,
-        getRoleById,
-        getRoles,
-        updateRole,
-        deleteRole,
-    }
-}
-
-const {createRole} = useRoles()
-
+export { createRole, deleteRole, getRoleById, getRoles, updateRole };
