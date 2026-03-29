c9b01130544b698fb8c4d543a1e40b5973553a7b
Mueed Ahmad <mueedahmad067@gmail.com>
2026-03-29 19:38:43 +0500

    feat: added the use-role-store

---
+++ frontend/src/features/roles/roles.store.ts
@@ -0,0 +1,20 @@
+import {create} from "zustand";
+import type {Role} from "@/features/roles/roles.types";
+
+type Modal = "edit" | "delete" | null;
+
+interface RoleStore {
+  open: Modal;
+  setOpen: (modal: Modal) => void;
+  role: Role | null;
+  setRole: (role: Role) => void;
+  clearRole: () => void;
+}
+
+export const useRoleStore = create<RoleStore>((set) => ({
+  clearRole: () => set({ role: null }),
+  open: null,
+  role: null,
+  setOpen: (modal) => set({ open: modal }),
+  setRole: (role) => set({ role }),
+}));
